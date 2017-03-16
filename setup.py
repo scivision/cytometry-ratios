faae037cc11e71632ebc618e06c497422d256326
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2017-03-15 23:58:26 -0400

    x

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ req = ['nose','numpy','matplotlib','seaborn','scikit-image','pathlib2']
 
 setup(name='pycyto',
       packages=['pycyto'],
-      url='https://github.com/scienceopen/cytometry-ratios',
+      url='https://github.com/scivision/cytometry-ratios',
       description='prototyping of whole slide cytometer',
       install_requires=req,
     )
