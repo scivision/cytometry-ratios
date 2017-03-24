63bb7c8f8eac3c8a8065fb07dd27ad28247a869a
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2017-03-24 17:00:30 -0400

    init

---
+++ PlotCV.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+"""
+Various plots for Coefficient of Variation, loaded from HDF5
+"""
+from pathlib import Path
+from numpy import median,arange,append
+from skimage.filters import gaussian
+from matplotlib.pyplot import figure,show
+import h5py
+import seaborn
+seaborn.set_style('ticks')
+
+LVL = append(arange(1.0,2.0,.1),arange(2.,4.,.2))
+#LVL=10
+
+def loadcv(fn):
+    fn = Path(fn).expanduser()
+    with h5py.File(fn,'r') as f:
+        for p in f: #for each variable in file
+            plotcv(f[p],p)
+        
+
+def plotcv(dat,name):
+    
+    D = gaussian(dat,17) #smoothed for contour
+    
+  
+    fg = figure()
+    ax = fg.gca()
+    hi=ax.imshow(dat,cmap='cubehelix_r',origin='bottom')
+    fg.colorbar(hi).set_label('CV %')
+    hc = ax.contour(D,LVL,colors='k')
+    ax.clabel(hc,fmt='%0.1f')
+    
+    ax.set_title(f'{name}\nmin(CV)={D.min():.2f} %'
+                 f'  median(CV)={median(D):.2f} %'
+                 f'  mean(CV)={D.mean():.2f} %')
+  
+
+
+    
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+    p = ArgumentParser()
+    p.add_argument('fn',help='HDF5 filename')
+    p = p.parse_args()
+    
+    loadcv(p.fn)
+    
+    show()
