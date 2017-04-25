63efc8b466c3768f5740f65cca47c4f8aef650cd
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2017-04-24 22:21:47 -0400

    handle sumix and basler

--- PlotCytoRawImages.py
+++ PlotCytoRawImages.py
@@ -4,9 +4,11 @@ Plots raw images from whole slide cytometer, more automated than just using Imag
 Michael Hirsch, Ph.D.
 
 ./PlotCytoRawImages.py ~/data/2017-04-24/basler-bare-sensor/
+./PlotCytoRawImages.py ~/data/2017-04-24/sumix-bare-sensor/
 """
 from pathlib import Path
 import numpy as np
+import scipy
 from skimage.filters import gaussian
 from matplotlib.pyplot import figure,show
 from mpl_toolkits.mplot3d import Axes3D
@@ -14,25 +16,39 @@ import seaborn as sns
 sns.set_style('ticks')
 import tifffile
 
-def plotimg(img, fn:Path):
-    D = gaussian(img,7)*4096 #smoothed for contour--output is normalized
+sigma=7
+
+def plotimg(img, fn:Path=None):
+    if img.dtype=='uint8':
+        bpp=8
+    else: # for basler dart ap0134 chip
+        bpp=12
+
+    D = gaussian(img,sigma)*(2**bpp-1) #smoothed for contour--output is normalized
 
     fg = figure(figsize=(12,5))
+    if isinstance(fn,Path):
+        fg.suptitle(f'{fn.name}', y=0.99)
+        zlbl = 'data numbers'
+    elif isinstance(fn,str):
+        fg.suptitle(fn, y=0.99)
+        zlbl = 'normalized'
+
     ax = fg.add_subplot(1,2,1)
     h = ax.imshow(D, cmap='cubehelix', origin='bottom')
-    fg.colorbar(h).set_label('data numbers')
+    fg.colorbar(h).set_label(zlbl)
     ax.set_xlabel('x-pixel')
     ax.set_ylabel('y-pixel')
-    fg.suptitle(f'{fn.name}')
-
-
+# %%
     x,y = np.meshgrid(range(img.shape[1]), range(img.shape[0]))
     ax = fg.add_subplot(1,2,2,projection='3d')
     #ax.plot_wireframe(x, y, D, rcount=20, ccount=20)
     ax.plot_surface(x, y, D, rcount=20, ccount=20,cmap='cubehelix')
     ax.set_xlabel('x-pixel')
     ax.set_ylabel('y-pixel')
-    ax.set_zlabel('data numbers')
+    ax.set_zlabel(zlbl)
+
+    fg.tight_layout()
 
 
 if __name__ == '__main__':
@@ -43,14 +59,24 @@ if __name__ == '__main__':
 
     fn = Path(p.fn).expanduser()
     if fn.is_dir():
-        flist = fn.glob('*.tif*')
+        flist = []
+        for e in ('*.tif*','*.bmp'):
+            flist += fn.glob(e)
     elif fn.is_file():
         flist = [fn]
     else:
         raise FileNotFoundError(f'{fn} not a file or directory')
 
     for fn in flist:
-        img = tifffile.imread(str(fn))
+        if fn.suffix.startswith('.tif'):
+            img = tifffile.imread(str(fn))
+        else:
+            img = scipy.ndimage.imread(fn)[1:-1,:,0]  # sumix stores all 3 RGB the same values for mono
         plotimg(img, fn)
+# %% impulse plot
+    imp = np.zeros((72, 72))
+    imp[imp.shape[0]//2, imp.shape[1]//2] = 1.
+
+    plotimg(gaussian(imp,sigma),'smoothing kernel')
 
     show()
