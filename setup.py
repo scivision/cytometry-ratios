527b070ff9522dd79921668ef17fb6a7d13b8e61
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-08-15 13:16:12 -0400

    setup template

--- setup.py
+++ setup.py
@@ -1,18 +1,3 @@
 #!/usr/bin/env python
-install_requires = ['numpy','matplotlib','seaborn','scikit-image','scipy','imageio','tifffile']
-tests_require=[]
-# %%
-from setuptools import setup, find_packages
-
-setup(name='pycyto',
-      packages=find_packages(),
-      author='Michael Hirsch, Ph.D.',
-      version='0.1.0',
-      url='https://github.com/scivision/cytometry-ratios',
-      description='prototyping of whole slide cytometer',
-      long_description=open('README.rst').read(),
-      install_requires=install_requires,
-      extras_require={'tests':tests_require},
-      tests_require=tests_require,
-      python_requires=">=3.5",
-    )
+from setuptools import setup
+setup()
